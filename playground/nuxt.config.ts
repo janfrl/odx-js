2bd961de106b3081e3fcbf97afda2662285f6af1
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2026-02-19 10:34:15 +0100

    test(playground): add devtools client subprocess for better DX

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,29 @@
+import { resolve } from 'node:path'
+import { defineNuxtModule } from '@nuxt/kit'
+import { startSubprocess } from '@nuxt/devtools-kit'
+
 export default defineNuxtConfig({
-  modules: ['../src/module'],
+  modules: [
+    '../src/module',
+    // Subprocess to start the devtools client server
+    defineNuxtModule({
+      setup(_, nuxt) {
+        if (!nuxt.options.dev) return
+
+        startSubprocess(
+          {
+            command: 'npx',
+            args: ['nuxi', 'dev', '--port', '3300'],
+            cwd: resolve(__dirname, '../client'),
+          },
+          {
+            id: 'sap-odata:client',
+            name: 'SAP OData Client Dev',
+          },
+        )
+      },
+    }),
+  ],
   devtools: { enabled: true },
   odata: {
     services: [
