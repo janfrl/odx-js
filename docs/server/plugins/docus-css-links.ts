19b4e5c929a8a3c31efe340350c2a4f4a12815dc
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2026-05-07 16:01:39 +0200

    docs: remove obsolete docus css workaround

--- docs/server/plugins/docus-css-links.ts
+++
@@ -1,7 +0,0 @@
-const WINDOWS_DOCUS_CSS_LINK_RE = /<link rel="stylesheet" href="\/_nuxt\/[A-Za-z]:\/[^"]+\/\.nuxt\/docus\.css" crossorigin>\n?/g
-
-export default defineNitroPlugin((nitroApp) => {
-  nitroApp.hooks.hook('render:html', (html) => {
-    html.head = html.head.map(entry => entry.replace(WINDOWS_DOCUS_CSS_LINK_RE, ''))
-  })
-})
